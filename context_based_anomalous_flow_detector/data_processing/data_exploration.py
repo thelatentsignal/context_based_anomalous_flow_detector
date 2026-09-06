56c225b319f206062a2b26f894fc6ff30e576fac
thelatentsignal <research@thelatentsignal.ai>
2026-09-06 22:41:25 +0200

    restructured such that I have the same workflow as in other projects, i.e. canonical data, preprocessed etc. The model can be trained;

--- context_based_anomalous_flow_detector/data_processing/data_exploration.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-df = pd.read_csv('../data/raw/f7546561558c07c5_NFV3DATA-A11964_A11964/data/NF-UNSW-NB15-v3.csv')
-#print(df.head())
-#print(df.tail())
-#print(df['Attack'].unique())
-#print(df[df['Attack']=="DoS"])
-
-df.columns
